f2d45e8668a625f8eeca79941198ae9ddac0751d
Vitoria <vitoria.machado@senior.com.br>
2024-08-07 14:51:42 -0300

    Ajustes LMS connection

--- src/scorm/LMS.ts
+++ src/scorm/LMS.ts
@@ -7,10 +7,18 @@ export class LMS {
   private static instance: LMS;
 
   static MAX_ATTEMPTS = 10;
-  static connection = import.meta.env.MODE === "production" ? startLMS() : true;
+  static connection: boolean | undefined = false; // Default to false initially
 
   static userLocation: number;
   static userData: unknown = {};
+
+  static async initialize() {
+    if (import.meta.env.MODE === "production") {
+      LMS.connection = await startLMS();
+    } else {
+      LMS.connection = true;
+    }
+  }
 
   static getInstance() {
     if (!LMS.instance) {
@@ -39,31 +47,43 @@ export class LMS {
       );
       LMS.userData = userData;
     } catch (e) {
-      console.error("Erro ao buscar userLocation: ", e);
+      console.error("Erro ao buscar userData: ", e);
     }
   }
 
-  async connect() {
+  async connect(): Promise<boolean> {
     let attempts = 0;
 
     while (!LMS.connection && attempts < LMS.MAX_ATTEMPTS) {
       try {
         LMS.connection = await startLMS();
+        if (LMS.connection) {
+          // Connection successful, exit loop
+          break;
+        }
         attempts++;
       } catch (e) {
         alert(`Erro ao tentar conectar na vez: ${attempts}`);
         alert(`Erro ocorrido: ${e}`);
+        attempts++;
       }
     }
 
-    if (attempts === LMS.MAX_ATTEMPTS) {
+    if (!LMS.connection) {
+      // If we exit the loop without a successful connection
       throw new Error("Falha na conexão ao LMS após múltiplas tentativas.");
     }
 
-    await LMS.settingLastPage();
-    await LMS.settingUserData();
-
-    return LMS.connection;
+    try {
+      // Perform additional setup if connection is established
+      await LMS.settingLastPage();
+      await LMS.settingUserData();
+      return true; // Connection was successful
+    } catch (e) {
+      // Handle any errors that occur during additional setup
+      console.error("Erro durante a configuração do LMS:", e);
+      return false;
+    }
   }
 
   get lastPage() {
